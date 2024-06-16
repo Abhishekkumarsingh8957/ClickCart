4469cc97d2bebc2554351adb08f83e677208276d
Vinay kumar <vin6287183337@gmail.com>
2024-06-16 15:28:51 +0530

    Implement login and register functionality on frontend and backend

--- models/userModel.js
+++ models/userModel.js
@@ -29,11 +29,6 @@ const userSchema = new mongoose.Schema({
         required:true
       },
 
-      answer:{
-        type:String,
-        required:true
-      },
-
       role:{
         type:Number,
         default:0
