bb59f956f984b8b85d14e82afe5dc8b428d88d52
Vinay kumar <vin6287183337@gmail.com>
2024-06-23 15:48:42 +0530

    Routes and controller For searching product Was created

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -9,6 +9,7 @@ import {
   productFiltersController,
   productCountController,
    productListController,
+   searchProductController,
 } from "../controllers/productController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 import formidable from "express-formidable";
@@ -53,6 +54,9 @@ router.get("/product-count", productCountController);
 
 //product per page
 router.get("/product-list/:page", productListController);
+
+//search product
+router.get("/search/:keyword", searchProductController);
 
 
 export default router;
