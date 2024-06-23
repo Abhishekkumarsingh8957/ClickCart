94c017c9caf1796b4b4ca7479ad1cddf3d671fce
Vinay kumar <vin6287183337@gmail.com>
2024-06-23 15:01:57 +0530

    Created routes and controllers forFiltering products

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,8 +5,7 @@ function HomePage() {
     const [auth, setAuth] = useAuth();
   return (
     <Layout>
-      <h1>HomePage</h1>
-        <pre>{JSON.stringify(auth, null, 4)}</pre>
+     
     </Layout>
   );
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,9 +6,13 @@ import {
   getSingleProductController,
   productPhotoController,
   updateProductController,
+  productFiltersController,
+  productCountController,
+   productListController,
 } from "../controllers/productController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 import formidable from "express-formidable";
+
 
 const router = express.Router();
 
@@ -40,5 +44,15 @@ router.get("/product-photo/:pid", productPhotoController);
 
 //delete rproduct
 router.delete("/delete-product/:pid", deleteProductController);
+
+//filter product
+router.post("/product-filters", productFiltersController);
+
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
 
 export default router;
