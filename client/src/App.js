e8a2799ee2fc7380a36b0d7f4ea4c1a891e4dc88
imdeepak2234 <imdeepak2234@gmail.com>
2024-06-27 21:00:31 +0530

    AdminOrders.js file updated and also it is added in authRoute and also controller created to handle this

--- client/src/App.js
+++ client/src/App.js
@@ -50,7 +50,7 @@ function App() {
           <Route path="admin/product/:slug" element={<UpdateProduct />} />
           <Route path="admin/products" element={<Products />} />
           <Route path="admin/users" element={<Users />} />
-          <Route path="admin/orders" element={<Users />} />
+          <Route path="admin/orders" element={<AdminOrders />} />
         </Route>
 
         <Route path="/register" element={<Register />} />
