99d1697f681bf704be66946c880aeb4d33ea8f9c
Gauravgkp12 <gg333591@gmail.com>
2024-06-28 14:28:14 +0530

    Modified about and contact section

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -13,13 +13,35 @@ const About = () => {
           />
         </div>
         <div className="col-md-4">
-          <p className="text-justify mt-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            officiis obcaecati esse tempore unde ratione, eveniet mollitia,
-            perferendis eius temporibus dicta blanditiis doloremque explicabo
-            quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
-            accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
-            commodi illum quidem neque tempora nam.
+          <p className="text-justify mt-2 text-align: center"  >
+          
+
+  <div>
+    <h4>About Us</h4>
+    <br/>
+    <p>
+      Welcome to CLICKCART, your ultimate destination for a seamless online shopping experience. 
+     
+    </p>
+    <p>
+      Our mission is to make shopping easy, enjoyable, and accessible for everyone. With a user-friendly 
+      interface, secure payment options, and prompt delivery services, we strive to ensure that every 
+      customer’s journey is smooth and satisfying.
+    </p>
+    <p>
+      At Clickcart, we believe in quality, affordability, and customer satisfaction. 
+    </p>
+    <p>
+      Join our community today and experience shopping like never before. Explore our wide range of products, 
+      take advantage of exclusive deals, and enjoy a hassle-free shopping experience, all from the comfort 
+      of your home.
+    </p>
+    <p>
+      Thank you for choosing Clickcart. We look forward to serving you!
+    </p>
+  </div>
+
+
           </p>
         </div>
       </div>
